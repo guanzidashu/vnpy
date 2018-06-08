89b640612565f3d93c19b0acf656151bb4614c93
张聪 <635518957@qq.com>
2018-06-08 23:58:59 +0800

    fix

--- examples/CtaBacktesting/runBacktesting.py
+++ examples/CtaBacktesting/runBacktesting.py
@@ -35,21 +35,21 @@ if __name__ == '__main__':
     # 设置使用的历史数据库
     engine.setDatabase(MINUTE_DB_NAME, 'IF0000')
 
-    d = {
-        'fastWindow':30,
-        "slowWindow":55,
-        }
-    print ("  "+'argument' + str(d))
+    # d = {
+    #     'fastWindow':30,
+    #     "slowWindow":55,
+    #     }
+    # print ("  "+'argument' + str(d))
 
-    engine.initStrategy(DoubleMaPosStrategy, d)
+    # engine.initStrategy(DoubleMaPosStrategy, d)
 
-    # 开始跑回测
-    engine.runBacktesting()
-    print("xxxxx")
-    # 显示回测结果
-    engine.showBacktestingResult()
+    # # 开始跑回测
+    # engine.runBacktesting()
+    # print("xxxxx")
+    # # 显示回测结果
+    # engine.showBacktestingResult()
 
-    engine.clearBacktestingResult()
+    # engine.clearBacktestingResult()
     
 
     
@@ -70,48 +70,50 @@ if __name__ == '__main__':
     #     engine.showBacktestingResult()
 
     #     engine.clearBacktestingResult()
-    # x = 10 
-    # y = 20
-    # step = 5
-    # dict = {}
-    # array = np.zeros((x-1,y-2))
-    # from vnpy.trader.app.ctaStrategy.strategy.strategyDoubleMa import DoubleMaStrategy
-    # for fastWindow in range(step,x*step,step):    
-    #     for slowWindow in range(fastWindow+step,y*step, step):
-    #         # 在引擎中创建策略对象
-    #         d = {
-    #             'fastWindow':fastWindow,
-    #             "slowWindow":slowWindow,
-    #             }
-    #         print ("  "+'argument' + str(d))
-
-    #         engine.initStrategy(DoubleMaPosStrategy, d)
-
-    #         # 开始跑回测
-    #         engine.runBacktesting()
-
-    #         # 显示回测结果
-    #         d_r = engine.showBacktestingResult()
-    #         dict[str(d)] = int(d_r['annualInterestRate']*100)
-    #         array[fastWindow/step-1][slowWindow/step-2] = int(d_r['annualInterestRate']*100)
-    #         engine.clearBacktestingResult()
-    #         print(dict)
 
 
-    # print(dict)
-    # import pandas as pd
-    # import matplotlib.pyplot as plt
-    # import seaborn as sns
-    # import json
+    x = 10 
+    y = 20
+    step = 5
+    dict = {}
+    array = np.zeros((x-1,y-2))
+    from vnpy.trader.app.ctaStrategy.strategy.strategyDoubleMa import DoubleMaStrategy
+    for fastWindow in range(step,x*step,step):    
+        for slowWindow in range(fastWindow+step,y*step, step):
+            # 在引擎中创建策略对象
+            d = {
+                'fastWindow':fastWindow,
+                "slowWindow":slowWindow,
+                }
+            print ("  "+'argument' + str(d))
+
+            engine.initStrategy(DoubleMaPosStrategy, d)
+
+            # 开始跑回测
+            engine.runBacktesting()
+
+            # 显示回测结果
+            d_r = engine.showBacktestingResult()
+            dict[str(d)] = int(d_r['annualInterestRate']*100)
+            array[fastWindow/step-1][slowWindow/step-2] = int(d_r['annualInterestRate']*100)
+            engine.clearBacktestingResult()
+            print(dict)
+
+
+    print(dict)
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    import json
 
 
 
-    # df = pd.DataFrame(array, columns=range(2*step, y*(step), step), index=range(step,x*(step),step))
+    df = pd.DataFrame(array, columns=range(2*step, y*(step), step), index=range(step,x*(step),step))
 
-    # sns.heatmap(df,annot=True)
-    # plt.savefig(str(x)+ "_"+str(y)+'DoubleMaPosStrategy.png')
+    sns.heatmap(df,annot=True)
+    plt.savefig(str(x)+ "_"+str(y)+'DoubleMaPosStrategy.png')
 
-    # plt.show()
+    plt.show()
 
 
 
